4540f6f22a962d2bd2ad9b6462451552a578c098
Juan Camilo Cuenca <camilocuenca1810@gmail.com>
2021-08-13 16:17:36 -0500

    Agregamos otro print

--- hola.java
+++ hola.java
@@ -2,6 +2,8 @@ package Program;
 public class hola{
 
     public static void main(String[] args) {
-        System.out.println("Hola Mundo :)");
+        System.out.println("Hola Mundo :)" );
+        System.out.println("que ondas gente ");
     }
+
 }
