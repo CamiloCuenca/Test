41c3d86a986ffc93bb529a53f9f5963a0db939f0
Juan Camilo Cuenca <camilocuenca1810@gmail.com>
2021-08-13 17:39:32 -0500

    variable numero y su print

--- hola.java
+++ hola.java
@@ -4,6 +4,10 @@ public class hola{
     public static void main(String[] args) {
         System.out.println("Hola Mundo :)" );
         System.out.println("que ondas gente ");
+        System.out.println("el Spreen pá");
+        int numero = 9;
+        System.out.println(numero);
+
     }
 
 }
