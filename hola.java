ad5a35621311d5ac07311fa96a2e86f37c6dcf70
Juan Camilo Cuenca <camilocuenca1810@gmail.com>
2021-08-13 20:48:59 -0500

    cambio de prints y uno mas

--- hola.java
+++ hola.java
@@ -3,13 +3,14 @@ public class hola{
 
     public static void main(String[] args) {
         System.out.println("Hola Mundo :)" );
-        System.out.println("que ondas gente ");
-        System.out.println("el Spreen pá");
+        System.out.println("Hello World");
+        System.out.println("Juan Camilo");
         int numero = 9;
         System.out.println(numero);
         double  numero2 = 9 ;
         System.out.println("la suma de los números es: " +( numero + numero2));
         System.out.println("------------------------------------------------");
+        System.out.println("Git");
 
     }
 
